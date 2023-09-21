0bc23f2a3d2fa2f7a5976c3470cd8351cc54e5b3
rohaib1 <rohaib192@gmail.com>
2023-09-21 16:26:14 +0100

    Initial commit

---
+++ src/main/java/com/bnta/capstoners/logistics_server/LogisticsServerApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.capstoners.logistics_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnta/capstoners/logistics_server/LogisticsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.capstoners.logistics_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
