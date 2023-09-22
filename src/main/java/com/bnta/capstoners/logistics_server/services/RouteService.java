aaad7b7e603a36b5e07a7767062ecc80ee38b1cc
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-22 11:15:23 +0100

    Created INDEX route for RouteController

--- src/main/java/com/bnta/capstoners/logistics_server/services/RouteService.java
+++ src/main/java/com/bnta/capstoners/logistics_server/services/RouteService.java
@@ -1,5 +1,19 @@
 package com.bnta.capstoners.logistics_server.services;
 
-public
-class RouteService{
+import com.bnta.capstoners.logistics_server.models.Route;
+import com.bnta.capstoners.logistics_server.repositories.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RouteService{
+
+        @Autowired
+        RouteRepository routeRepository;
+
+        public List<Route> findAllRoutes(){
+                return routeRepository.findAll();
         }
+}
