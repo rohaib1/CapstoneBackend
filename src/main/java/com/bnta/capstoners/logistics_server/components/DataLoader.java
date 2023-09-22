2a6f275c40604da332237f19e531bcd5dabcf3bc
rohaib1 <rohaib192@gmail.com>
2023-09-22 15:50:45 +0100

    Fixed route_id not showing on orders table

--- src/main/java/com/bnta/capstoners/logistics_server/components/DataLoader.java
+++ src/main/java/com/bnta/capstoners/logistics_server/components/DataLoader.java
@@ -15,8 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-public
-class DataLoader implements ApplicationRunner {
+public class DataLoader implements ApplicationRunner {
     @Autowired
     OrderRepository orderRepository;
 
@@ -26,18 +25,20 @@ class DataLoader implements ApplicationRunner {
     @Autowired
     VanRepository vanRepository;
 
-    public
-    DataLoader(){
-
+    public DataLoader() {
     }
 
     @Override
-    public
-    void run(ApplicationArguments args) throws Exception{
+    public void run(ApplicationArguments args) throws Exception {
+
+        Route route = new Route("Route 001", null, 51.500832, -0.124638);
+
+
+        routeRepository.save(route);
+
         List<Order> orders = Arrays.asList(
                 new Order(51.501277, -0.140780, "Hairdryer", LocalDate.of(2023, 9, 8)),
                 new Order(51.501307, -0.092973, "Toaster", LocalDate.of(2023, 9, 8)),
-                new Order(51.513297, -0.159024, "Coffee Maker", LocalDate.of(2023, 9, 9)),
                 new Order(51.536149, -0.154428, "Blender", LocalDate.of(2023, 9, 10)),
                 new Order(51.529774, -0.128646, "Vacuum Cleaner", LocalDate.of(2023, 9, 11)),
                 new Order(51.528213, -0.102621, "Hair Straightener", LocalDate.of(2023, 9, 12)),
@@ -45,12 +46,16 @@ class DataLoader implements ApplicationRunner {
                 new Order(51.498431, -0.061480, "Kettle", LocalDate.of(2023, 9, 14)),
                 new Order(51.486531, -0.088410, "Iron", LocalDate.of(2023, 9, 15)),
                 new Order(51.479693, -0.141626, "Toaster", LocalDate.of(2023, 9, 16))
-        );
-        orderRepository.saveAll(orders);
 
-        List<Route> routes = Arrays.asList(
-                new Route("Route 001", orders, 51.500832, -0.124638)
         );
-        routeRepository.saveAll(routes);
+
+        for (Order order : orders) {
+            order.setRoute(route);
+        }
+
+
+        orderRepository.saveAll(orders);
+        route.setOrders(orders);
+        routeRepository.save(route);
     }
 }
