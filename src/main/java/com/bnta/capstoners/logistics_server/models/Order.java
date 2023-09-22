0f015bf476e72e8e20a185c708315f6edddc9374
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-09-22 10:51:10 +0100

    populated order class

--- src/main/java/com/bnta/capstoners/logistics_server/models/Order.java
+++ src/main/java/com/bnta/capstoners/logistics_server/models/Order.java
@@ -1,5 +1,104 @@
 package com.bnta.capstoners.logistics_server.models;
 
-public
-class Order {
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "route_id")
+    @JsonIgnoreProperties({"orders"})
+    private Route route;
+
+    @Column
+    private double lng;
+
+    @Column
+    private double lat;
+
+    @Column
+    private String description;
+
+    @Column
+    private LocalDate date;
+
+    @Column
+    private boolean delivered;
+
+    public Order( double lng, double lat, String description, LocalDate date) {
+        this.route = null;
+        this.lng = lng;
+        this.lat = lat;
+        this.description = description;
+        this.date = date;
+        this.delivered = false;
+    }
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public boolean isDelivered() {
+        return delivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        this.delivered = delivered;
+    }
 }
