4185fcb86435e380491bd4ff0a39d68abfb095da
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-22 11:56:50 +0100

    order service layer complete

--- src/main/java/com/bnta/capstoners/logistics_server/services/OrderService.java
+++ src/main/java/com/bnta/capstoners/logistics_server/services/OrderService.java
@@ -1,5 +1,32 @@
 package com.bnta.capstoners.logistics_server.services;
 
-public
-class OrderService {
+import com.bnta.capstoners.logistics_server.models.Order;
+import com.bnta.capstoners.logistics_server.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Order> findAllOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findOrderById(Long id){
+        return orderRepository.findById(id);
+    }
+
+    public Order updateOrderStatus(Long id, boolean status){
+        Order orderToUpdate = orderRepository.findById(id).get();
+        orderToUpdate.setDelivered(status);
+        orderRepository.save(orderToUpdate);
+        return orderToUpdate;
+    }
+
 }
